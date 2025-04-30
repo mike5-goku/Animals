13abf50c1abcdf45a9bc34982fcbaf3022c4a7fb
mike5-goku <dylanmacias173@gmail.com>
2025-04-30 13:51:26 -0700

    modificacion a app.css y app.jxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
+import './App.css'
+import AnimalsShow from './AnimalsShow'
+function getRandomAnimal (){
+const animals = ['cat','bird','cow','dog','gator','horse']
+return animals[Math.random()* animals.length]
+
+}
 
 
 function App() {
-  
+const [animals, setAnimals] = useState([])  
+const handleClick = () =>{
+setAnimals([...animals, getRandomAnimal()])
 
-  return (
-    <>
-     <h1>Animals</h1>
-     
-    </>
-  )
 }
+const renderedAnimals = animals.map((animal, index) =>{
+return <AnimalsShow type={animal} key={index}/>
+
+
+})
+return(
+<div className ='app'>   
+  <button onClick={handleClick}>Add animal</button>
+  <div className="animals-list">
+  {renderedAnimals}
+</div>
+</div>
+)
+
+}
+
+
 
 export default App
