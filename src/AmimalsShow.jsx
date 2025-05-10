985cde285db152faf615a343dd30c44afffb00d1
mike5-goku <dylanmacias173@gmail.com>
2025-05-09 17:03:16 -0700

    hola

--- src/AmimalsShow.jsx
+++ src/AmimalsShow.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import bird from '/assets/svg/bird.svg'
-import dog from '/assets/svg/dog.svg'
-import cat from '/assets/svg/cat.svg'
-import cow from '/assets/svg/cow.svg'
-import gator from '/assets/svg/gator.svg'
-import heart from '/assets/svg/heart.svg'
-import horse from '/assets/svg/horse.svg'
+import bird from './assets/svg/bird.svg'
+import dog from './assets/svg/dog.svg'
+import cat from './assets/svg/cat.svg'
+import cow from './assets/svg/cow.svg'
+import gator from './assets/svg/gator.svg'
+import heart from './assets/svg/heart.svg'
+import horse from './assets/svg/horse.svg'
 import './AnimalsShow.css'
 
 const svgMap ={
